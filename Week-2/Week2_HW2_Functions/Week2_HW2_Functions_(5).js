bfb8e2e525d683cbb87496704565db1d64bc4325
Hector Orozco <hectorjoseorozco@gmail.com>
2021-08-07 16:31:19 -0500

    Make all functions "arrow functions".

--- Week-2/Week2_HW2_Functions/Week2_HW2_Functions_(5).js
+++ Week-2/Week2_HW2_Functions/Week2_HW2_Functions_(5).js
@@ -1,13 +1,7 @@
 // 5. Write a function that returns the number of arguments passed to the function when called.
 
-/**
- * Function that returns the number of arguments passed to the function when called.
- * @returns {Number} The number of arguments passed to the function when called.
- */
-function getNumberOfArguments() {
-    return arguments.length;
-}
+const getNumberOfArguments = (...arguments) => arguments.length;
 
 // Code that uses "numberOfArguments" function.
 let numberOfArguments = getNumberOfArguments(1, 2, 3, 4, 5, 6);
-console.log(`\nThe number of arguments passed into the function when called like "getNumberOfArguments(1,2,3,4,5,6)" is: ${numberOfArguments}.`);
+console.log(`\nThe number of arguments passed into the function when called as "getNumberOfArguments(1,2,3,4,5,6)" is: ${numberOfArguments}.`);
--- Week-2/Week2_HW2_Functions/Week2_HW2_Functions_Arrays_(4,7,8).js
+++ Week-2/Week2_HW2_Functions/Week2_HW2_Functions_Arrays_(4,7,8).js
@@ -1,96 +1,34 @@
-// 4. Define a function sumArray and a function multiplyArray that sums and multiplies (respectively) all the numbers in an array of numbers. For example, 
-// sumArray([1,2,3,4]) should return 10, and multiplyArray([1,2,3,4]) should return 24.
+/* 4. Define a function sumArray and a function multiplyArray that sums and multiplies (respectively) all the numbers in an array of numbers. For example, 
+      sumArray([1,2,3,4]) should return 10, and multiplyArray([1,2,3,4]) should return 24. */
 
-/**
- * Function that returns the sum of all the numbers in an array.
- * @param {Array} numbersArray An array of numbers. 
- * @returns {Number} The sum of all the numbers in the array. For example, sumArray([1,2,3,4]) should return 10.
- */
-function sumArray(numbersArray) {
-    let sum = 0;
-    for (let i = 0; i < numbersArray.length; i++) {
-        sum += numbersArray[i];
-    }
-    return sum;
-}
+const sumArray = numbersArray => numbersArray.reduce((accumulator, currentValue) => accumulator + currentValue);
 
-/**
- * Function that returns the multiplication of all the numbers in an array.
- * @param {Array} numbersArray An array of numbers.
- * @returns {Number} The multiplication of all the numbers in the array. For example, multiplyArray([1,2,3,4]) should return 24.
- */
-function multiplyArray(numbersArray) {
-    let multiplication = 1;
-    for (let i = 0; i < numbersArray.length; i++) {
-        multiplication *= numbersArray[i];
-    }
-    return multiplication;
-}
+const multiplyArray = numbersArray => numbersArray.reduce((accumulator, currentValue) => accumulator * currentValue);
 
-// Code that uses "sumArray" and "multiplyArray" functions.
+// Code that uses the funtions "sumArray" and "multiplyArray":
 let array = [1, 2, 3, 4];
-let sum = sumArray(array);
-let multiplication = multiplyArray(array);
-console.log(`\nThe array of numbers is [${array}]. The sum of its elements is ${sum}, and the multiplication of its elements is ${multiplication}.`)
+array.length > 0 ?
+    console.log(`\nThe array of numbers is [${array}]. The sum of its elements is ${sumArray(array)}, and the multiplication of its elements is ${multiplyArray(array)}.`) :
+    console.log("The array provided is empty. No sum or multiplication of its elements can be performed.");
 
 // **************************************************************************************************************************
 
 // 7. Write a function findLongestWord that takes an array of words and returns the length of the longest one.
 
-/**
- * Function that returns the length of the longest word in an array.
- * @param {Array} wordsArray An array of words (Strings).
- * @returns {Number} The length of the longest word (String) in the array. -1 if the array is empty.
- */
-function findLongestWord(wordsArray) {
-    if (wordsArray.length === 0) {
-        return -1;
-    } else if (wordsArray.length === 1) {
-        return wordsArray[0].length;
-    } else {
-        // Compares the length of the first element with the length of the second, then the greater of those two lengths 
-        // with the length of the third element, and so on until the end of the array.
-        let longestLength = wordsArray[0].length;
-        for (let i = 1; i < wordsArray.length; i++) {
-            if (wordsArray[i].length > longestLength) {
-                longestLength = wordsArray[i].length;
-            }
-        }
-        return longestLength;
-    }
-}
+const findLongestWord = wordsArray => wordsArray.reduce((accumulator, currentValue) => Math.max(accumulator, currentValue.length), wordsArray[0].length);
 
-// Code that uses "findLongestWord" function.
+// Code that uses the funtion "findLongestWord":
 let wordsArray = ["apple", "banana", "cucumber", "pineapple", "onion"];
-let longestLength = findLongestWord(wordsArray);
-if (longestLength < 0) {
-    console.log("\nThe array is empty.");
-} else {
-    console.log(`\nThe length of the longest word in [${wordsArray}] is ${longestLength}.`);
-}
+wordsArray.length > 0 ? console.log(`\nThe length of the longest word in [${wordsArray}] is ${findLongestWord(wordsArray)}.`) :
+    console.log("\nThe array provided is empty.");
 
 // **************************************************************************************************************************
 
 // 8. Write a function filterLongWords that takes an array of words and a number i and returns the array of words that are longer than i characters long.
 
-/**
- * Function that selects words with a length greater than a given number in an array.
- * @param {Array} wordsArray An array of words (Strings) 
- * @param {Number} i The number that will filter the words. 
- * @returns {Array} Array of words (Strings) with a length greater that "i" 
- */
-function filterLongWords(wordsArray, i) {
-    let wordsLonger = [];
-    for (let j = 0; j < wordsArray.length; j++) {
-        if (wordsArray[j].length > i) {
-            wordsLonger.push(wordsArray[j]);
-        }
-    }
-    return wordsLonger;
-}
+const filterLongWords = (wordsArray, i) => wordsArray.reduce((accumulator, currentValue) => (currentValue.length > i) ? accumulator.concat(currentValue) : accumulator, new Array());
 
-// Code that uses "filterLongWords" function.
-wordsArray = ["apple", "banana", "cucumber", "pineapple", "onion"];
-let i = 6;
-let longWords = filterLongWords(wordsArray, i);
-console.log(`\nThe words with a length greater than ${i} in [${wordsArray}] are [${longWords}].`);
+// Code that uses the funtion "filterLongWords":
+wordsArray = ["cat", "lion", "dolphin", "elephant", "dog"];
+let i = 3;
+console.log(`\nThe words with a length greater than ${i} in [${wordsArray}] are [${filterLongWords(wordsArray, i)}].`);
